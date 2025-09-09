63783dd62644e175ad0e0602766b49c1699d6b50
JakeMazz-Code <jake@jmazzarella.com>
2025-09-09 18:47:00 -0400

    Added Python main file, requirements.txt, and .gitignore

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Main Python file for Assignment2
+"""
+
+def main():
+    print("Hello, World!")
+    print("This is Assignment2 Python file")
+
+if __name__ == "__main__":
+    main()
+
